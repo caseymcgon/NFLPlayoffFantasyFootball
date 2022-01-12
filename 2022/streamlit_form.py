d0a267dd4e1af3fbca2c53e4f671081ead70dae9
Casey McGonigle <cmcgon@berkeley.edu>
2022-01-11 20:15:01 -0800

    changed to public_gsheets_url

--- 2022/streamlit_form.py
+++ 2022/streamlit_form.py
@@ -20,7 +20,7 @@ def make_form():
         rows = conn.execute(query, headers=1)
         return rows
 
-    sheet_url = st.secrets["gsheets_url"]
+    sheet_url = st.secrets["public_gsheets_url"]
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
     # Print results.
