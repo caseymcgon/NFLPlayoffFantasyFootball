2378b72af81cee6f350fca41fd80308348ed394f
Casey McGonigle <cmcgon@berkeley.edu>
2024-01-07 17:40:49 -0800

    added logic to check past deadline

--- utils/ui_utils.py
+++ utils/ui_utils.py
@@ -4,41 +4,101 @@ import time
 import pytz
 import streamlit as st
 import numpy as np
+from datetime import datetime
+import pytz
 """
 This module provides utility functions for the streamlit user interface.
 """
 
-def display_start_date_countdown(start_date_deadline_str, year):
-        # Calculate a "Days, Hours, Mins, Seconds To"/"Days, Hours, Mins, Second Since" start_date_deadline and display it on the page using streamlit
+def display_deadline_message(start_date_deadline_str, year):
+     
+    # Convert the string to UTC datetime object 
+    start_date_deadline_utc = get_utc_datetime(start_date_deadline_str)
+    diff, before_deadline_bool = compute_time_till_deadline(start_date_deadline_utc)
 
-        # Remove the day of the week from the string
-        start_date_deadline = start_date_deadline_str.split(' (')[0]
+    # Convert the difference to days and hours
+    days = diff.days
+    hours = diff.seconds // 3600
+    minutes = (diff.seconds // 60) % 60
+    # seconds = diff.seconds % 60
 
-        # Convert the string to a datetime object
-        start_date_deadline = datetime.strptime(start_date_deadline, '%Y-%m-%d %I:%M%p %Z')
+    # Display the result
+    if before_deadline_bool:
+        pst_timezone = pytz.timezone('US/Pacific')
+        day_of_week = check_day_of_week(start_date_deadline_utc.astimezone(pst_timezone))
+        st.markdown(f"""
+                ---
+                ##### **{day_of_week} {start_date_deadline_str} PST** 
+                is the deadline to submit your team (in the **Roster Input** tab on the Left Side)
 
-        # Convert the datetime object to UTC
-        start_date_deadline = start_date_deadline.astimezone(pytz.UTC)
+                You may submit as many times as you'd like before the deadline.
+                Only your final submission before the deadline will be counted.
+            """)
+    else:
+        st.markdown(f"""
+                ---
+                Uh oh! The first NFL playoff games of {year} kicked off at {start_date_deadline_str}
 
-        # Get the current time in UTC
-        now = datetime.now(pytz.UTC)
+                You may no longer submit a team this year :( -- Hope you'll join us next year
+         """)
+        
+def compute_time_till_deadline(start_date_deadline_utc):
+    """ Returns (diff, bool) 
+        - diff = datetime time difference between now and deadline
+    ##  - bool =  True if now is before the deadline
+    """
+    # Get the current time in UTC
+    now = datetime.now(pytz.UTC)
 
-        # Calculate the difference
-        diff = start_date_deadline - now
+    # Calculate the difference
+    diff = start_date_deadline_utc - now
 
-        # Convert the difference to days and hours
-        days = diff.days
-        hours = diff.seconds // 3600
-        minutes = (diff.seconds // 60) % 60
-        seconds = diff.seconds % 60
+    # Check if the deadline has passed
+    if diff.total_seconds() <= 0:
+        return diff, False
+    else:
+        return diff, True
+    
+def get_utc_datetime(start_date_deadline_str):
+    # Convert the string to a datetime object
+    start_date_deadline = datetime.strptime(start_date_deadline_str, '%b %d, %Y %I:%M %p')
+    
+    ## localize the timezone-naive object to PST then cast to UTC
+    pst_timezone = pytz.timezone('US/Pacific')
+    start_date_deadline_utc = pst_timezone.localize(start_date_deadline).astimezone(pytz.utc)
 
-        # Display the result
-        if np.abs(days) > 7:
-            start_date_deadline_str = start_date_deadline_str.split(' (')[0] ## day of week extra clutter more than a week out away
-        if diff.total_seconds() > 0:
-            st.write(f"""# {days} days {hours} hours, {minutes} mins til the {year} NFL Playoffs Kick Off ({start_date_deadline_str})""")
-        else:
-            st.write(f"""{-days} days, {hours} hours, {minutes} mins since the {year} NFL Playoffs Kicked Off ({start_date_deadline_str})""")
+    return start_date_deadline_utc
+    
+def check_day_of_week(start_date_deadline):
+    # Convert the string to a datetime object
+    
+
+    # Get the day of the week
+    day_of_week = start_date_deadline.strftime('%A')
+
+    return day_of_week
+
+
+# def display_start_date_countdown(start_date_deadline_str, year):
+#        ## Useful if counting down seconds til kickoff of first game
+
+#         # Convert the string to UTC datetime object 
+#         start_date_deadline = get_utc_datetime(start_date_deadline_str)
+
+#         diff, before_deadline_bool = time_till_deadline(start_date_deadline)
+
+#         # Convert the difference to days and hours
+#         days = diff.days
+#         hours = diff.seconds // 3600
+#         minutes = (diff.seconds // 60) % 60
+#         seconds = diff.seconds % 60
+
+#         # Display the result
+#         if before_deadline_bool:
+#             day_of_week = check_day_of_week(start_date_deadline_str)
+#             st.write(f"""# {days} days {hours} hours, {minutes} mins til the {year} NFL Playoffs Kick Off: {start_date_deadline_str}, {day_of_week} """)
+#         else:
+#             st.write(f"""{-days} days, {hours} hours, {minutes} mins since the {year} NFL Playoffs Kicked Off ({start_date_deadline_str})""")
 
 
         ## If you wanted to make live updating time (seconds, it'd look something like this)
@@ -60,8 +120,10 @@ def display_start_date_countdown(start_date_deadline_str, year):
         #     time.sleep(1)
         #     st.rerun()
 
-def check_past_deadline(start_date_deadline_str):
-    pass
+
+# start_date_deadline_str = "Jan 15, 2024 1:30 PM PST"
+# day_of_week = check_day_of_week(start_date_deadline_str)
+# print(day_of_week)
 
 def function2():
     # Function 2 implementation
