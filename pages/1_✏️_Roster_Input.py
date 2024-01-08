948dd3ae5555dccc2e2d4621a863b26590353799
Casey McGonigle <cmcgon@berkeley.edu>
2024-01-08 00:51:34 -0800

    added check for missing fields and added team outputs to bottom of page

--- pages/1_✏️_Roster_Input.py
+++ pages/1_✏️_Roster_Input.py
@@ -13,7 +13,7 @@ def make_form():
 
     # from utils import ui_utils
     import Playoff_Fantasy_Overview
-    from utils import ui_utils
+    from utils import ui_utils, roster_manager
 
     # Load the yearly_settings.json file
     with open('yearly_settings.json', 'r') as yearly_settings:
@@ -29,19 +29,11 @@ def make_form():
         roster_google_sheet_name = year_settings.get('roster_google_sheet_name')
         buy_in = year_settings.get('buy_in')
 
-    deadline = f"**Deadline: {start_date_deadline_str} PST.**"
+    deadline = f"**Deadline: {start_date_deadline_str} PST**"
 
-    # CONNECT TO GOOGLE SHEETS (where I store the roster info)
-    # credit: https://www.codeforests.com/2020/11/22/gspread-read-write-google-sheet/
-    scopes = [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive'
-    ]
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"], scopes=scopes)
-    client = gspread.authorize(creds)
-    google_sh = client.open(roster_google_sheet_name)
-    sheet1 = google_sh.get_worksheet(0)
+    google_sheet = roster_manager.access_sheet_in_drive(roster_google_sheet_name)
+
+    sheet1 = google_sheet 
 
     ## Add Collapsible Rules section
     expander = st.expander("What are the Rules again?")
@@ -82,7 +74,7 @@ def make_form():
         tiecols = st.columns(3)
         champ = tiecols[0].text_input("First Tiebreaker: Super Bowl Champ")
         runner = tiecols[1].text_input("Second Tiebreaker: Super Bowl Runner-Up")
-        score = tiecols[2].slider("Third Tiebreaker: Super Bowl Total Points", min_value=2, max_value=120, value=60)
+        score = tiecols[2].slider("Third Tiebreaker: Super Bowl Total Points", min_value=2, max_value=120, value=2)
 
         form.write("""---""")
         # Section for the Submit Button
@@ -102,41 +94,43 @@ def make_form():
         now =  datetime.now().astimezone(pst_timezone)
         entry_time = datetime.strftime(now, "%Y-%m-%d %H:%M:%S")
 
-
-        if before_deadline_bool:
-            sheet1.append_rows(values=[[f"{name}", f"{entry_time}" , f"{qb1}", f"{qb2}",
-                                        f"{k1}", f"{k2}", f"{d1}", f"{d2}",
-                                        f"{p1}", f"{p2}", f"{p3}", f"{p4}",
-                                        f"{p5}", f"{p6}", f"{p7}",
-                                        f"{champ}", f"{runner}", f"{score}"]
-                                    ], insert_data_option="INSERT_ROWS")
-            st.write(f"#### please venmo {buy_in} @cmcgo")
-            st.title("Team submitted Successfully. Good Luck!!")
-            # st.write(f"{sheet1.get_all_values()}")
-        # MESSAGE FOR PAST DEADLINE
-        else:
+        roster_output = [f"{name}", f"{entry_time}" , f"{qb1}", f"{qb2}",
+                        f"{k1}", f"{k2}", f"{d1}", f"{d2}",
+                        f"{p1}", f"{p2}", f"{p3}", f"{p4}",
+                        f"{p5}", f"{p6}", f"{p7}",
+                        f"{champ}", f"{runner}", f"{score}"
+                    ]
+        if '' in roster_output:
+            st.title("Oops. You're missing a field! Please try again")
+        elif not before_deadline_bool:
             st.title("Sorry! It's past the Deadline!")
+        else:
+            sheet1.append_rows(values=[roster_output], insert_data_option="INSERT_ROWS")
+            st.write(f"#### please venmo {buy_in} @cmcgo")
+            st.title(f"Team Submitted Successfully")
 
+            # Allow user to see their team
+            st.write("**Quarterbacks**")
+            st.write(f'   1. {qb1} 2. {qb2}')
+
+            st.write("**Kickers**")
+            st.write(f'   1. {k1} 2. {k2}')
+
+            st.write("**Defenses**")
+            st.write(f'1. {d1} 2.  {d2}')
+
+            st.write("**Position Players**")
+            st.write(f'1. {p1} 2.  {p2} 3. {p3} 4. {p4} 5. {p5} 6. {p6} 7. {p7}')
+
+            st.write("**Tie-Breakers**")
+            st.write(f'1. SB Champ: {champ}')
+            st.write(f'2. SB Runner-Up: {runner}')
+            st.write(f'3. SB Total Points: {score}')
+
+            st.title(f"Good Luck, {name}!!")
+            st.write(f"Please don't forget to venmo")
+        
         return 
-        # Allow user to see their team
-        st.title(f"{name}'s Team")
-
-        st.write("**Quarterbacks**")
-        st.write(f'   1. {qb1} 2. {qb2}')
-
-        st.write("**Kickers**")
-        st.write(f'   1. {k1} 2. {k2}')
-
-        st.write("**Defenses**")
-        st.write(f'1. {d1} 2.  {d2}')
-
-        st.write("**Position Players**")
-        st.write(f'1. {p1} 2.  {p2} 3. {p3} 4. {p4} 5. {p5} 6. {p6} 7. {p7}')
-
-        st.write("**Tie-Breakers**")
-        st.write(f'1. SB Champ: {champ}')
-        st.write(f'2. SB Runner-Up: {runner}')
-        st.write(f'3. SB Total Points: {score}')
 
         st.write("**Submitted @**: ", entryTime, "pst")
 
