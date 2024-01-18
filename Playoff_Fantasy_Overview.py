e3181253cd23b805268021abf65d933963f06913
Casey McGonigle <cmcgon@berkeley.edu>
2024-01-18 02:00:34 -0800

    add WC weekend write-up

--- Playoff_Fantasy_Overview.py
+++ Playoff_Fantasy_Overview.py
@@ -36,14 +36,21 @@ def show_league_info(selected_year):
         
         st.markdown(
             f"""
-            The 2024 NFL Playoffs have *finally* arrived. And that means we get 1 last chance at Fantasy Fooball glory this season.
+           2024 Wild Card Weekend ✅. And just like that...down go the Dallas Cowboys. For the 30th time in the last 30 years, America's Team will not be competing in the NFC's Conference Championship Game and that just breaks this Niners fan's heart...but actually it kinda does -- at the last moment I added 3 'Boys to my roster, as did MANY others (26/34 of us picked CeeDee, 19 Aubrey, 13 DAL D, 11 Dak...).
 
-            The goal is simple: pick the guys you think are going to find pay-dirt the most times between now & Super Bowl Sunday.
+            Anyway, it's still early, and almost anything can happen from here on out. We'll learn a lot more about who's got a shot at this thing after this weekend's matchups (Texans @ Ravens & Packers @ Niners on Sat; Bucs @ Lions & Chiefs @ Bills on Sunday). 
 
-            Best of luck!
+            For the time being, Bills Mafia Cardholder Will Grant (Kelly's SCU buddy) has 90 points and a 10 point lead over the pack (thanks in part to the Josh Allen <> Khalil Shakir connection), but he's got a load of dead dudes (Dak, Ferguson, Puka, Aubrey, DAL, CeeDee, Raheem) and has earned himself the ignominious distinction of being the first ever participant in our little playoff pool to select a player (Leonard Fournette) who was subsequently released from his NFL team! So that's 8/13 players out after 1 week...yikes. Wilch, next year please remember: in this league, the best ability is *availability*.
 
-            ---
-            #### ↖️ Choose Your Team in the Roster Input Tab
+            Elsewhere, 4 of the top 5 (Stroud, Mayfield, HOU D, and Love -- all moving on to the Divisional Round) are unowned, but 17 of us are on Josh Allen's league leading 28 points. Other missed opportunities include the 3 unowned Defenses (HOU : 21 pts, GB : 11 pts, TB: 2 pts) that scored this weekend. On the other hand 5 CLE owners and 13 DAL owners went out out scoreless.
+
+            On a non-football note: please leave some feedback & features requests for things you'd like to be able to do in the app! I am still adding new features for this year and would love to incorporate your ideas. And as always, please audit the scorekeeping and let me know if your team looks wrong to you somehow. I changed up a lot of the backend infrastructure this year in an effort to automate a lot of the work (we should get score updates every 30 minutes during this weekend's games), but that work is certainly bug-prone. Shoot me an email or text if anything looks fishy to you.
+
+            Thanks again for playing & see you next week. In the meantime, Go Niners!
+
+            Go Niners!
+            - Casey
+
             ---
             """
         )
