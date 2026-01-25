b94fda19a351192997815ffce67303ac3149b2f2
Casey McGonigle <cmcgon@berkeley.edu>
2026-01-24 20:26:20 -0800

    I can't figure out how to give Rashid Shaheed his points for his return but otherwise, we're ready for Conference Championships

--- Playoff_Fantasy_Overview.py
+++ Playoff_Fantasy_Overview.py
@@ -1,7 +1,7 @@
 ## Playoff_Fantasy.py -- the main / landing page for the app
 
 SELECTED_YEAR = '2026' ## key to access values from yearly_settings.json
-PAT_UPDATE_NOTE = "PATs updated through WC Weekend (no Divisional yet)"
+PAT_UPDATE_NOTE = "PATs updated through Divisional round (no Conference Championships) yet)"
 
 def show_league_info(current_year):
     import streamlit as st
